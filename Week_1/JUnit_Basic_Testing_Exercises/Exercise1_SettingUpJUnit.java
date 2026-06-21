e7d882c714c99aea888cf8f96ff306bc08346933
Swetha G <144980486+swethaganesh5@users.noreply.github.com>
2026-06-22 01:07:43 +0530

    Update Exercise1_SettingUpJUnit.java

--- Week_1/JUnit_Basic_Testing_Exercises/Exercise1_SettingUpJUnit.java
+++ Week_1/JUnit_Basic_Testing_Exercises/Exercise1_SettingUpJUnit.java
@@ -1,1 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+public class Exercise1_SettingUpJUnit {
+
+    @Test
+    void testAddition() {
+        int result = 2 + 3;
+        assertEquals(5, result);
+    }
+}

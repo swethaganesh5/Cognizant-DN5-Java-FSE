834255f71439e876d0635fbddd974e9ac4a22b43
Swetha G <144980486+swethaganesh5@users.noreply.github.com>
2026-06-22 01:12:23 +0530

    Add control structure example for number evaluation

--- Week_1/PLSQL_Exercises/ControlStructures.sql
+++ Week_1/PLSQL_Exercises/ControlStructures.sql
@@ -1,1 +1,13 @@
 
+DECLARE
+    num NUMBER := 10;
+BEGIN
+    IF num > 0 THEN
+        DBMS_OUTPUT.PUT_LINE('Number is Positive');
+    ELSIF num < 0 THEN
+        DBMS_OUTPUT.PUT_LINE('Number is Negative');
+    ELSE
+        DBMS_OUTPUT.PUT_LINE('Number is Zero');
+    END IF;
+END;
+/

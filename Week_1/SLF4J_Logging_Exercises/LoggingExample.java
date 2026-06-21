198dce4d33fe9eeb05179850a47076370cde1968
Swetha G <144980486+swethaganesh5@users.noreply.github.com>
2026-06-22 01:11:40 +0530

    Update LoggingExample.java

--- Week_1/SLF4J_Logging_Exercises/LoggingExample.java
+++ Week_1/SLF4J_Logging_Exercises/LoggingExample.java
@@ -1,1 +1,18 @@
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoggingExample {
+
+    private static final Logger logger =
+            LoggerFactory.getLogger(LoggingExample.class);
+
+    public static void main(String[] args) {
+
+        logger.info("This is an INFO message");
+
+        logger.warn("This is a WARNING message");
+
+        logger.error("This is an ERROR message");
+    }
+}

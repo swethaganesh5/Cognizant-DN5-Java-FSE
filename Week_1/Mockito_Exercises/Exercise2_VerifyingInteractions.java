13af77e374cdcbfac6163c3776c3a451cca9a118
Swetha G <144980486+swethaganesh5@users.noreply.github.com>
2026-06-22 01:10:40 +0530

    Update Exercise2_VerifyingInteractions.java

--- Week_1/Mockito_Exercises/Exercise2_VerifyingInteractions.java
+++ Week_1/Mockito_Exercises/Exercise2_VerifyingInteractions.java
@@ -1,1 +1,20 @@
 
+import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.*;
+
+interface NotificationService {
+    void sendNotification(String message);
+}
+
+public class Exercise2_VerifyingInteractions {
+
+    @Test
+    void testVerifyInteraction() {
+
+        NotificationService mockService = mock(NotificationService.class);
+
+        mockService.sendNotification("Hello");
+
+        verify(mockService).sendNotification("Hello");
+    }
+}

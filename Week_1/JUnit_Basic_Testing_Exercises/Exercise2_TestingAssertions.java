bd6dbb47aa69e59484f8b55831ebeb0c0ca7e048
Swetha G <144980486+swethaganesh5@users.noreply.github.com>
2026-06-22 01:08:26 +0530

    Update Exercise2_TestingAssertions.java

--- Week_1/JUnit_Basic_Testing_Exercises/Exercise2_TestingAssertions.java
+++ Week_1/JUnit_Basic_Testing_Exercises/Exercise2_TestingAssertions.java
@@ -1,1 +1,20 @@
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Exercise2_TestingAssertions {
+
+    @Test
+    void testAssertions() {
+
+        assertEquals(10, 5 + 5);
+
+        assertTrue(10 > 5);
+
+        assertFalse(5 > 10);
+
+        assertNull(null);
+
+        assertNotNull("JUnit");
+    }
+}
